446e484e8bcf17c546247ccd5ab1bd5f25514f64
prismatic-koi <ben@tinfoilforest.nz>
2026-03-11 15:23:03 +1300

    neovim: add Māori spellcheck dictionary via fetchurl

--- modules/programs/prism/neovim/default.nix
+++ modules/programs/prism/neovim/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -17,22 +18,36 @@
     ./plugins
   ];
   config = lib.mkIf config.nx.programs.prism.neovim.enable {
-    home-manager.users.${config.nx.username} = {
-      programs.neovim = {
-        enable = true;
-        defaultEditor = true;
+    home-manager.users.${config.nx.username} =
+      let
+        maorispl = pkgs.fetchurl {
+          url = "https://ftp.nluug.nl/pub/vim/runtime/spell/mi.utf-8.spl";
+          hash = "sha256:0qr0szs8mfhm90k02z2pab3n5s93pl9ysfsz53b64dr4rkdqys4w";
+        };
+        maoriSpellDir = pkgs.runCommand "maori-spell" { } ''
+          mkdir -p $out/spell
+          cp ${maorispl} $out/spell/mi.utf-8.spl
+        '';
+      in
+      {
+        programs.neovim = {
+          enable = true;
+          defaultEditor = true;
+          initLua = lib.mkAfter ''
+            vim.opt.runtimepath:append("${maoriSpellDir}")
+          '';
+        };
+        home.persistence."/persist" = {
+          directories = [
+            ".config/nvim/spell"
+            ".local/share/nvim"
+            ".local/state/nvim"
+          ];
+        };
+        xdg.mimeApps.defaultApplications = lib.mkIf config.programs.neovim.enable {
+          "text/plain" = "nvim.desktop";
+          "text/markdown" = "nvim.desktop";
+        };
       };
-      home.persistence."/persist" = {
-        directories = [
-          ".config/nvim/spell"
-          ".local/share/nvim"
-          ".local/state/nvim"
-        ];
-      };
-      xdg.mimeApps.defaultApplications = lib.mkIf config.programs.neovim.enable {
-        "text/plain" = "nvim.desktop";
-        "text/markdown" = "nvim.desktop";
-      };
-    };
   };
 }
