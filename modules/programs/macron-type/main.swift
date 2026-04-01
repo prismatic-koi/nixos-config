142efa079baff7718eaa5aee7440ea432e313d8e
prismatic-koi <ben@tinfoilforest.nz>
2026-04-02 09:22:58 +1300

    fix(macron-type): accept plain ASCII vowel arg, map to macron internally
    Avoids UTF-8 characters in karabiner.json shell_command strings which
    may cause encoding issues. macron-type now takes a|e|i|o|u|A|E|I|O|U
    and looks up the corresponding macron codepoint internally.

--- modules/programs/macron-type/main.swift
+++ modules/programs/macron-type/main.swift
@@ -1,19 +1,27 @@
 import CoreGraphics
 import Foundation
 
+let macrons: [Character: UniChar] = [
+    "a": 0x0101, // ā
+    "e": 0x0113, // ē
+    "i": 0x012B, // ī
+    "o": 0x014D, // ō
+    "u": 0x016B, // ū
+    "A": 0x0100, // Ā
+    "E": 0x0112, // Ē
+    "I": 0x012A, // Ī
+    "O": 0x014C, // Ō
+    "U": 0x016A, // Ū
+]
+
 guard CommandLine.arguments.count == 2,
-      let scalar = CommandLine.arguments[1].unicodeScalars.first
+      let key = CommandLine.arguments[1].first,
+      let uchar = macrons[key]
 else {
-    fputs("usage: macron-type <character>\n", stderr)
+    fputs("usage: macron-type <a|e|i|o|u|A|E|I|O|U>\n", stderr)
     exit(1)
 }
 
-guard scalar.value <= 0xFFFF else {
-    fputs("error: character must be in the Basic Multilingual Plane (U+0000–U+FFFF)\n", stderr)
-    exit(1)
-}
-
-let uchar = UniChar(scalar.value)
 var keyDown = CGEvent(keyboardEventSource: nil, virtualKey: 0, keyDown: true)!
 var keyUp   = CGEvent(keyboardEventSource: nil, virtualKey: 0, keyDown: false)!
 keyDown.keyboardSetUnicodeString(stringLength: 1, unicodeString: [uchar])
