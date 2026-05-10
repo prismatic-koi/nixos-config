5a3dab32c1490b810bbc5cab8057e73d918224c4
Ben Sherman <ben@tinfoilforest.nz>
2026-05-11 10:40:18 +1200

    Move atlassian site and email settings to sops secrets (#1551)
    This change migrates the Atlassian site and email configuration from plain-text Nix options into the existing sops-nix secrets file. This improves security by ensuring sensitive account identifiers are encrypted alongside the existing API token.

--- modules/programs/atlassian/default.nix
+++ modules/programs/atlassian/default.nix
@@ -13,16 +13,6 @@ in
       enable = lib.mkEnableOption "enables the atlassian CLI" // {
         default = true;
       };
-      site = lib.mkOption {
-        type = lib.types.str;
-        default = "tinfoilforest.atlassian.net";
-        description = "Atlassian cloud site hostname (e.g. mycompany.atlassian.net)";
-      };
-      email = lib.mkOption {
-        type = lib.types.str;
-        default = "ben@tinfoilforest.nz";
-        description = "Atlassian account email address";
-      };
     };
   };
 
@@ -42,6 +32,16 @@ in
       # Linux: sops secret via system config
       (lib.mkIf isLinux {
         sops.secrets = {
+          "atlassian_site" = {
+            owner = username;
+            mode = "0600";
+            sopsFile = ../secrets/atlassian.sops.yaml;
+          };
+          "atlassian_email" = {
+            owner = username;
+            mode = "0600";
+            sopsFile = ../secrets/atlassian.sops.yaml;
+          };
           "atlassian_token" = {
             owner = username;
             mode = "0600";
@@ -50,8 +50,8 @@ in
         };
 
         environment.sessionVariables = {
-          ATLASSIAN_SITE = config.nx.programs.atlassian.site;
-          ATLASSIAN_EMAIL = config.nx.programs.atlassian.email;
+          ATLASSIAN_SITE = "$(cat ${config.sops.secrets.atlassian_site.path})";
+          ATLASSIAN_EMAIL = "$(cat ${config.sops.secrets.atlassian_email.path})";
           ATLASSIAN_API_TOKEN = "$(cat ${config.sops.secrets.atlassian_token.path})";
         };
       })
@@ -60,14 +60,22 @@ in
       (lib.mkIf isDarwin {
         home-manager.users.${username} = {
           sops.secrets = {
+            "atlassian_site" = {
+              sopsFile = ../secrets/atlassian.sops.yaml;
+            };
+            "atlassian_email" = {
+              sopsFile = ../secrets/atlassian.sops.yaml;
+            };
             "atlassian_token" = {
               sopsFile = ../secrets/atlassian.sops.yaml;
             };
           };
 
           home.sessionVariables = {
-            ATLASSIAN_SITE = config.nx.programs.atlassian.site;
-            ATLASSIAN_EMAIL = config.nx.programs.atlassian.email;
+            ATLASSIAN_SITE = "$(cat ${config.home-manager.users.${username}.sops.secrets.atlassian_site.path})";
+            ATLASSIAN_EMAIL = "$(cat ${
+              config.home-manager.users.${username}.sops.secrets.atlassian_email.path
+            })";
             ATLASSIAN_API_TOKEN = "$(cat ${
               config.home-manager.users.${username}.sops.secrets.atlassian_token.path
             })";
